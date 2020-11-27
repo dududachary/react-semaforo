242817d2afd2e8cdda40ce736c79eef757c68299
Eduardo Schiavo Dachary <eduardo.dachary@gmail.com>
2020-11-27 09:19:24 -0300

    Criada classe Lampada

---
+++ src/lampada.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.css';
+
+class Lampada extends React.Component {
+  render () {
+    return (
+      <div>
+        Lâmpada
+      </div>
+    )
+  }
+}
+
+export default Lampada;
--- src/semaforo.jsx
+++ src/semaforo.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
+import Lampada from './lampada.jsx';
 
 class Semaforo extends React.Component {
   render () {
     return (
       <div>
-        Semaforo
+        <Lampada cor='Vermelho'/>
+        <Lampada cor='Amarelo'/>
+        <Lampada cor='Verde'/>
       </div>
     )
   }
